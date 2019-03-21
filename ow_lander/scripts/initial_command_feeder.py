b5cfa22561ea00da47feacc5eb665fa3ddb2b72a
Terry Welsh <terence.m.welsh@nasa.gov>
2019-03-21 11:32:59 -0700

    Fixed syntax in script.

--- ow_lander/scripts/initial_command_feeder.py
+++ ow_lander/scripts/initial_command_feeder.py
@@ -9,15 +9,18 @@
 import sys
 import os
 import rospy
+import time
 
 def main():
+  time.sleep(3)
+
   # Publish initial position command
-  os.system("rostopic pub -1 /shou_yaw_position_controller/command std_msgs/Float64 -1.5&")
-  os.system("rostopic pub -1 /shou_pitch_position_controller/command std_msgs/Float64 1.5708&")
-  os.system("rostopic pub -1 /prox_pitch_position_controller/command std_msgs/Float64 -2.75&")
-  os.system("rostopic pub -1 /dist_pitch_position_controller/command std_msgs/Float64 3.1416&")
-  os.system("rostopic pub -1 /hand_yaw_position_controller/command std_msgs/Float64 0&")
-  os.system("rostopic pub -1 /scoop_yaw_position_controller/command std_msgs/Float64 0&")
+  os.system('rostopic pub -1 /shou_yaw_position_controller/command std_msgs/Float64 "data: -1.5" &')
+  os.system('rostopic pub -1 /shou_pitch_position_controller/command std_msgs/Float64 "data: 1.5708" &')
+  os.system('rostopic pub -1 /prox_pitch_position_controller/command std_msgs/Float64 "data: -2.75" &')
+  os.system('rostopic pub -1 /dist_pitch_position_controller/command std_msgs/Float64 "data: 3.1416" &')
+  os.system('rostopic pub -1 /hand_yaw_position_controller/command std_msgs/Float64 "data: 0" &')
+  os.system('rostopic pub -1 /scoop_yaw_position_controller/command std_msgs/Float64 "data: 0" &')
 
 if __name__ == '__main__':
   main()
