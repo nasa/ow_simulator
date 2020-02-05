8281e1e8d344f28d9f0111f03370edd351bbb56f
anjan <anjan.chakrabarty@nasa.gov>
2020-02-04 23:37:53 -0800

    add trench digging trajectory

---
+++ ow_lander/scripts/activity_reset.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+# __BEGIN_LICENSE__
+# Copyright (c) 2018-2019, United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration. All
+# rights reserved.
+# __END_LICENSE__
+
+import constants
+import math
+
+## GLOBAL VARS ##
+J_SCOOP_YAW = 5
+J_HAND_YAW = 4
+J_DIST_PITCH = 3 
+J_PROX_PITCH = 2
+J_SHOU_PITCH = 1
+J_SHOU_YAW = 0
+
+def arg_parsing(req):
+  if req.use_defaults :
+    # Default trenching values
+    trench_x=1.5
+    trench_y=0
+    trench_d=0.02
+    delete_prev_traj=False
+
+  else :
+    trench_x=req.trench_x
+    trench_y=req.trench_y
+    trench_d=req.trench_d
+    delete_prev_traj=req.delete_prev_traj
+
+  return [req.use_defaults,trench_x,trench_y,trench_d,delete_prev_traj]
+
+def dig_trench(move_arm,move_limbs,x_tr, y_tr, depth):
+  # Compute shoulder yaw angle to trench
+  alpha = math.atan2(y_tr-constants.Y_SHOU, x_tr-constants.X_SHOU)
+  h = math.sqrt( pow(y_tr-constants.Y_SHOU,2) + pow(x_tr-constants.X_SHOU,2) )
+  l = constants.Y_SHOU - constants.HAND_Y_OFFSET
+  beta = math.asin (l/h)
+
+  # Move to pre trench position, align shoulder yaw
+  joint_goal = move_arm.get_current_joint_values()
+  joint_goal[constants.J_DIST_PITCH] = 0
+  joint_goal[constants.J_HAND_YAW] = math.pi/2.2
+  joint_goal[constants.J_PROX_PITCH] = -math.pi/2
+  joint_goal[constants.J_SHOU_PITCH] = math.pi/2
+  joint_goal[constants.J_SHOU_YAW] = alpha + beta
+  
+  # If out of joint range, abort (TODO: parse limit from urdf)
+  if (joint_goal[constants.J_SHOU_YAW]<-1.8) or (joint_goal[constants.J_SHOU_YAW]>1.8): 
+    return False
+
+  joint_goal[constants.J_SCOOP_YAW] = 0
+  move_arm.go(joint_goal, wait=True)
+  move_arm.stop()
+
+  # Once aligned to trench goal, place hand above trench middle point
+  goal_pose = move_limbs.get_current_pose().pose
+  goal_pose.position.x = x_tr
+  goal_pose.position.y = y_tr
+  goal_pose.position.z = constants.GROUND_POSITION + constants.SCOOP_OFFSET - depth
+  move_limbs.set_pose_target(goal_pose)
+  plan = move_limbs.plan()
+
+  if len(plan.joint_trajectory.points) == 0: # If no plan found, abort
+    return False
+
+  plan = move_limbs.go(wait=True)
+  move_limbs.stop()
+  move_limbs.clear_pose_targets()
+
+  # Rotate hand yaw to dig in
+  joint_goal = move_arm.get_current_joint_values()
+  joint_goal[constants.J_HAND_YAW] = 0
+  move_arm.go(joint_goal, wait=True)
+  move_arm.stop()
+
+  # Insert here for linear trenching
+
+  # Rotate hand yaw to dig out
+  joint_goal = move_arm.get_current_joint_values()
+  joint_goal[constants.J_HAND_YAW] = -math.pi/2.2
+  move_arm.go(joint_goal, wait=True)
+  move_arm.stop()
+
+  # # Go back to safe position and align yaw to deliver
+  # joint_goal = move_arm.get_current_joint_values()
+  # joint_goal[constants.J_DIST_PITCH] = 0
+  # joint_goal[constants.J_HAND_YAW] = -math.pi/2
+  # joint_goal[constants.J_PROX_PITCH] = -math.pi/2
+  # joint_goal[constants.J_SHOU_PITCH] = math.pi/2
+  # joint_goal[constants.J_SHOU_YAW] = constants.SHOU_YAW_DELIV
+  # joint_goal[constants.J_SCOOP_YAW]= 0
+  # move_arm.go(joint_goal, wait=True)
+  # move_arm.stop()
+
+  # # Go to deliver position
+  # joint_goal = move_arm.get_current_joint_values()
+  # joint_goal[constants.J_PROX_PITCH]= math.pi/2 - 0.1
+  # joint_goal[constants.J_SCOOP_YAW]= math.pi - 0.05
+  # move_arm.go(joint_goal, wait=True)
+  # move_arm.stop()
+
+  # # Deliver (high amplitude)
+  # joint_goal = move_arm.get_current_joint_values()
+  # joint_goal[constants.J_HAND_YAW] = -math.pi
+  # move_arm.go(joint_goal, wait=True)
+  # move_arm.stop()
+  # joint_goal[constants.J_HAND_YAW] = math.pi/2
+  # move_arm.go(joint_goal, wait=True)
+  # move_arm.stop()
+  return True
+
+def go_home(move_arm):
+  # Move to home position
+  joint_goal = move_arm.get_current_joint_values()
+  joint_goal[J_DIST_PITCH] = 3.1416
+  joint_goal[J_HAND_YAW] = 0
+  joint_goal[J_PROX_PITCH] = -2.75
+  joint_goal[J_SHOU_PITCH] = 1.5708
+  joint_goal[J_SHOU_YAW] = -1.5
+  joint_goal[J_SCOOP_YAW] = 0
+  move_arm.go(joint_goal, wait=True)
+  move_arm.stop()
+
---
+++ ow_lander/scripts/move_group_python_interface_anjan.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2013, SRI International
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of SRI International nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Acorn Pooley, Mike Lautman
+
+## BEGIN_SUB_TUTORIAL imports
+##
+## To use the Python MoveIt interfaces, we will import the `moveit_commander`_ namespace.
+## This namespace provides us with a `MoveGroupCommander`_ class, a `PlanningSceneInterface`_ class,
+## and a `RobotCommander`_ class. More on these below. We also import `rospy`_ and some messages that we will use:
+##
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+## END_SUB_TUTORIAL
+
+
+def all_close(goal, actual, tolerance):
+  """
+  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
+  @param: goal       A list of floats, a Pose or a PoseStamped
+  @param: actual     A list of floats, a Pose or a PoseStamped
+  @param: tolerance  A float
+  @returns: bool
+  """
+  all_equal = True
+  if type(goal) is list:
+    for index in range(len(goal)):
+      if abs(actual[index] - goal[index]) > tolerance:
+        return False
+
+  elif type(goal) is geometry_msgs.msg.PoseStamped:
+    return all_close(goal.pose, actual.pose, tolerance)
+
+  elif type(goal) is geometry_msgs.msg.Pose:
+    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+
+  return True
+
+
+class MoveGroupPythonIntefaceTutorial(object):
+  """MoveGroupPythonIntefaceTutorial"""
+  def __init__(self):
+    super(MoveGroupPythonIntefaceTutorial, self).__init__()
+    
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+    
+    group_name = "arm"
+    group = moveit_commander.MoveGroupCommander(group_name)
+
+    robot = moveit_commander.RobotCommander()
+
+    scene = moveit_commander.PlanningSceneInterface()
+
+    #move_arm = moveit_commander.MoveGroupCommander("arm")
+    move_arm = moveit_commander.MoveGroupCommander(group_name)
+    move_limbs = moveit_commander.MoveGroupCommander("limbs")
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                   moveit_msgs.msg.DisplayTrajectory,
+                                                   queue_size=20)
+    self.move_arm = move_arm
+    self.move_limbs = move_limbs
+    '''
+
+    group_name = "arm"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+    '''
+
+
+    ## ^^^^^^^^^^^^^^^^^^^^^^^^^
+    # We can get the name of the reference frame for this robot:
+    planning_frame = move_arm.get_planning_frame()
+    print "============ Planning frame: %s" % planning_frame
+
+    # We can also print the name of the end-effector link for this group:
+    eef_link = move_group.get_end_effector_link()
+    print "============ End effector link: %s" % eef_link
+
+    # We can get a list of all the groups in the robot:
+    group_names = robot.get_group_names()
+    print "============ Available Planning Groups:", robot.get_group_names()
+
+    # Sometimes for debugging it is useful to print the entire state of the
+    # robot:
+    print "============ Printing robot state"
+    print robot.get_current_state()
+    print ""
+    ## END_SUB_TUTORIAL
+
+    # Misc variables
+    self.box_name = ''
+    self.robot = robot
+    self.scene = scene
+    self.move_group = move_group
+    self.display_trajectory_publisher = display_trajectory_publisher
+    self.planning_frame = planning_frame
+    self.eef_link = eef_link
+    self.group_names = group_names
+
+
+
+
+
+def main():
+  try:
+    print ""
+    print "----------------------------------------------------------"
+    print "Welcome to the MoveIt MoveGroup Python Interface Tutorial"
+    print "----------------------------------------------------------"
+    print "Press Ctrl-D to exit at any time"
+    print ""
+    print "============ Press `Enter` to begin the tutorial by setting up the moveit_commander ..."
+    raw_input()
+    tutorial = MoveGroupPythonIntefaceTutorial()
+
+
+  except rospy.ROSInterruptException:
+    return
+  except KeyboardInterrupt:
+    return
+
+if __name__ == '__main__':
+  main()
+
+
+ 
