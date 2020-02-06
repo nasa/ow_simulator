09cd7ed333d7fde18d043a939794d6b1d24e9734
Molly O'Connor <molly.c.oconnor@nasa.gov>
2020-02-05 17:12:02 -0800

    Added SOC_msg capability and a (possibly misguided CMakeLists.txt into src, hoping to link to rest of node tomorrow. Builds.

--- ow_power_system/src/power_system_node.cpp
+++ ow_power_system/src/power_system_node.cpp
@@ -14,13 +14,23 @@
 int main(int argc, char* argv[]) {
   
   ros::init(argc,argvf,"power_system_node");
-  ros::NodeHandle nhandle ("power_system_node");
+  ros::NodeHandle n ("power_system_node");
+
+  //Construct our State of Charge (SOC) publisher
+  ros::Publisher SOC_pub = n.advertise<std_msgs::Float64>("state_of_charge",1000);
 
   // ROS Loop. Note that once this loop starts,
   // this function (and node) is terminated with an interrupt.
-
   ros::Rate rate(1); // 1 Hz, tailor as needed
   while (ros::ok()) {
+    //individual soc_msg to be published by SOC_pub
+    std_msgs::Float64 soc_msg;
+
+    soc_msg.data=0.0; //zero for testing purposes, will be calculated later
+
+    //publish current SOC
+    SOC_pub.publish(soc_msg);
+    
     ros::spinOnce();
     rate.sleep();
     ROS_INFO ("Power system node running");
