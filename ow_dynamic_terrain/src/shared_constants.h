e6db639b2cc71f1a437376573ebf6402c651e6ac
Ussama Naal <ussama.naal@nasa.gov>
2020-04-22 08:31:43 -0700

    Added a world based of europa workspace

--- ow_dynamic_terrain/src/shared_constants.h
+++ ow_dynamic_terrain/src/shared_constants.h
@@ -3,6 +3,6 @@ class SharedConstants
 public:
     static const int WARM_UP_PERIOD_IN_SECONDS = 5;  // Time to wait for plugins to fully load
 
-    static constexpr double DRILL_POINT_X = +310.0;
-    static constexpr double DRILL_POINT_Y = -275.0;
+    static constexpr double DRILL_POINT_X = 0.0;
+    static constexpr double DRILL_POINT_Y = 0.0;
 };
