aa74a9761b9c7bcc87710f42d6388b8427420dae
Ussama Naal <ussama.naal@nasa.gov>
2020-07-13 08:56:33 -0700

    Add comments and a little refactor +  reduce absolute epsilon to 1.e-4

--- ow_dynamic_terrain/scripts/modify_terrain_grinder_pub.py
+++ ow_dynamic_terrain/scripts/modify_terrain_grinder_pub.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+"""
+The modify_terrain_grinder_pub module updates the terrain in line with the  
+grinder movement. It accomplishes this by subscribing to link poses of the grinder
+and issuing a corresponding modify_terrain_* message to update the terrain.
+"""
+
 import rospy
 import time
 from math import degrees
@@ -18,7 +24,7 @@ class ModifyTerrainGrinder:
     self.pub = rospy.Publisher(
         'ow_dynamic_terrain/modify_terrain_circle', modify_terrain_circle, queue_size=1)
     self.states_sub = rospy.Subscriber(
-        "/gazebo/link_states", LinkStates, self.emit_circle_message_callback)
+        "/gazebo/link_states", LinkStates, self.handle_link_states)
 
   def compose_modify_terrain_circle_message(self, position):
     return modify_terrain_circle(position=Point(position.x, position.y, position.z),
@@ -28,36 +34,36 @@ class ModifyTerrainGrinder:
   def check_and_submit(self, new_position, new_rotation):
     """ Checks if position has changed significantly before submmitting a message """
 
-    current_translation = np.array(
-        [new_position.x, new_position.y, new_position.z])
-
-    if all(np.isclose(current_translation, self.last_translation, atol=1.e-5)):
-      return  # no significant change, abort
-
     _, roll, _ = euler_from_quaternion(
         quaternion=(new_rotation.x, new_rotation.y, new_rotation.z, new_rotation.w))
 
     current_orientation = np.array([degrees(roll)])
     effective_orientation = np.array([90])
     if not all(np.isclose(current_orientation, effective_orientation, atol=10)):
-      return
+      return # grinder not aligned for digging, abort
+
+    current_translation = np.array([new_position.x, new_position.y, new_position.z])
+    if all(np.isclose(current_translation, self.last_translation, atol=1.e-4)):
+      return  # no significant change, abort
+
+
 
     self.last_translation = current_translation
 
     msg = self.compose_modify_terrain_circle_message(new_position)
     self.pub.publish(msg)
     
-    rospy.logdebug_throttle(1, "modify_terrain_scoop message:\n" + str(msg))
+    rospy.logdebug_throttle(1, "modify_terrain_grinder message:\n" + str(msg))
 
-  def emit_circle_message_callback(self, data):
+  def handle_link_states(self, data):
     try:
       idx = data.name.index("lander::l_grinder")
     except ValueError:
+      rospy.logerr_throttle(1, "lander::l_grinder not found in link_states")
       return
 
     position = data.pose[idx].position
     orientation = data.pose[idx].orientation
-
     self.check_and_submit(position, orientation)
 
 
--- ow_dynamic_terrain/scripts/modify_terrain_scoop_pub.py
+++ ow_dynamic_terrain/scripts/modify_terrain_scoop_pub.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+"""
+The modify_terrain_scoop_pub module updates the terrain in line with the scoop
+movement. It accomplishes this by subscribing to link poses of the scoop and
+issuing a corresponding modify_terrain_* message to update the terrain.
+"""
+
 import time
 from math import degrees
 import rospy
@@ -18,7 +24,7 @@ class ModifyTerrainScoop:
     self.pub = rospy.Publisher(
         'ow_dynamic_terrain/modify_terrain_ellipse', modify_terrain_ellipse, queue_size=1)
     self.states_sub = rospy.Subscriber(
-        "/gazebo/link_states", LinkStates, self.emit_ellipse_message_callback)
+        "/gazebo/link_states", LinkStates, self.handle_link_states)
 
   def compose_modify_terrain_ellipse_message(self, position, orientation):
     return modify_terrain_ellipse(position=Point(position.x, position.y, position.z),
@@ -30,10 +36,8 @@ class ModifyTerrainScoop:
   def check_and_submit(self, new_position, new_rotation):
     """ Checks if position has changed significantly before submmitting a message """
 
-    current_translation = np.array(
-        [new_position.x, new_position.y, new_position.z])
-
-    if all(np.isclose(current_translation, self.last_translation, atol=1.e-5)):
+    current_translation = np.array([new_position.x, new_position.y, new_position.z])
+    if all(np.isclose(current_translation, self.last_translation, atol=1.e-4)):
       return  # no significant change, abort
 
     self.last_translation = current_translation
@@ -43,12 +47,14 @@ class ModifyTerrainScoop:
 
     msg = self.compose_modify_terrain_ellipse_message(new_position, degrees(yaw))
     self.pub.publish(msg)
+    
     rospy.logdebug_throttle(1, "modify_terrain_scoop message:\n" + str(msg))
 
-  def emit_ellipse_message_callback(self, data):
+  def handle_link_states(self, data):
     try:
       idx = data.name.index("lander::l_scoop_tip")
     except ValueError:
+      rospy.logerr_throttle(1, "lander::l_scoop_tip not found in link_states")
       return
 
     position = data.pose[idx].position
