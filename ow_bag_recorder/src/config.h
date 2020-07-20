b4bbb20fc0521dcad59a46b8fb3a94f36599299a
Thomas Stucky <thomas.stucky@nasa.gov>
2020-07-20 19:28:01 -0400

    First draft of bag_recorder_node  - Options are exposed to user via YAML file  - Topics are specified by user via another YAML file

---
+++ ow_bag_recorder/src/config.h
@@ -0,0 +1,15 @@
+// The Notices and Disclaimers for Ocean Worlds Autonomy Testbed for Exploration
+// Research and Simulation can be found in README.md in the root directory of
+// this repository.
+
+// rosbag includes
+#include <rosbag/recorder.h>
+
+#include <vector>
+
+// retrieve recorder options from Parameter Server
+// use existing settings in out_options as defaults
+bool get_recorder_options(rosbag::RecorderOptions &out_options);
+
+// retrieve topics to be recorded from Parameter Server
+void get_recorder_topics(std::vector<std::string> &out_topics);
