40d28aab3e8a972b857b2f899aaf4a6f3c2ed3e1
anjan <anjan.chakrabarty@nasa.gov>
2020-09-22 15:23:49 -0700

    python changes

--- ow_lander/scripts/mechanical_power_arm.py
+++ ow_lander/scripts/mechanical_power_arm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import rospy
 from sensor_msgs.msg import JointState
@@ -8,47 +8,43 @@ from std_msgs.msg import Float64
 import sys, time
 import numpy as np
 
-#history = []
-
 VERBOSE=True
 
 class MechanicalPower:
-    def __init__(self):
-        '''Initialize ros publisher, ros subscriber'''
-        # topic where we publish
-        self.power_pub = rospy.Publisher('/mechanical_power/raw', Float64, queue_size = 10)
-        self.power_pub_a = rospy.Publisher('/mechanical_power/average', Float64, queue_size = 10)
+  def __init__(self):
+    '''Initialize ros publisher, ros subscriber'''
+    # topic where we publish
+    self.power_pub = rospy.Publisher('/mechanical_power/raw', Float64, queue_size = 10)
+    self.power_pub_a = rospy.Publisher('/mechanical_power/average', Float64, queue_size = 10)
         
-        self.subscriber = rospy.Subscriber('/joint_states', JointState, self.callback, queue_size = 1)
-        self.history = []
-        rate = rospy.Rate(100)    
+    self.subscriber = rospy.Subscriber('/joint_states', JointState, self.callback, queue_size = 1)
+    self.history = []
+    rate = rospy.Rate(100)    
             
 
-
-
-    def _moving_average(self, msg):
-        #global history
-        self.history.append(msg)
-        if len(self.history) > 10:
-            self.history = self.history[-10:]
-            average = sum(self.history) / float(len(self.history))
-            #rospy.loginfo('Average of most recent {} samples: {}'.format(len(self.history), average))
-        else:
-            average = msg
-        return average    
+  def _moving_average(self, msg):
+    #global history
+    self.history.append(msg)
+    if len(self.history) > 10:
+        self.history = self.history[-10:]
+        average = sum(self.history) / float(len(self.history))
+        #rospy.loginfo('Average of most recent {} samples: {}'.format(len(self.history), average))
+    else:
+        average = msg
+    return average    
         
-    def callback(self, ros_data):
-        self._value = len(ros_data.name)
-        l = len(ros_data.name)
-        power = 0
-        for x in range(0,l):
-            power = power + abs(ros_data.velocity[x]*ros_data.effort[x])
+  def callback(self, ros_data):
+    self._value = len(ros_data.name)
+    num_joints = len(ros_data.name)
+    power = 0
+    for x in range(0,num_joints):
+        power = power + abs(ros_data.velocity[x]*ros_data.effort[x])
 
         # Publish total power
-        power_avg =  self._moving_average(power)
+    power_avg =  self._moving_average(power)
         
-        self.power_pub.publish(power)
-        self.power_pub_a.publish(power_avg)
+    self.power_pub.publish(power)
+    self.power_pub_a.publish(power_avg)
 
 def main(args):
     '''Initializes and cleanup ros node'''
