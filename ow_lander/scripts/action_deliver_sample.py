6ee41c9de9305fc5113892b5ba22ebfcae90178e
anjan1984 <anjan1984@gmail.com>
2021-04-15 11:47:53 -0700

    fix different tab size

--- ow_lander/scripts/action_deliver_sample.py
+++ ow_lander/scripts/action_deliver_sample.py
@@ -49,45 +49,43 @@ def calculate_joint_state_end_pose_from_plan_arm (robot, plan, move_arm, moveit_
 
 
 def cascade_plans (plan1, plan2):
-     # Create a new trajectory object
-    new_traj = RobotTrajectory()
-    # Initialize the new trajectory to be the same as the planned trajectory
-    traj_msg = JointTrajectory()
-    # Get the number of joints involved
-    n_joints1 = len(plan1.joint_trajectory.joint_names)
-    n_joints2 = len(plan2.joint_trajectory.joint_names)
-    # Get the number of points on the trajectory
-    n_points1 = len(plan1.joint_trajectory.points)
-    n_points2 = len(plan2.joint_trajectory.points)
-    # Store the trajectory points
-    points1 = list(plan1.joint_trajectory.points)
-
-    points2 = list(plan2.joint_trajectory.points)
-    end_time = plan1.joint_trajectory.points[n_points1-1].time_from_start
-    start_time =  plan1.joint_trajectory.points[0].time_from_start
-    duration =  end_time - start_time
-    # add a time toleracne between  successive plans
-    time_tolerance = rospy.Duration.from_sec(0.1)
+  # Create a new trajectory object
+  new_traj = RobotTrajectory()
+  # Initialize the new trajectory to be the same as the planned trajectory
+  traj_msg = JointTrajectory()
+  # Get the number of joints involved
+  n_joints1 = len(plan1.joint_trajectory.joint_names)
+  n_joints2 = len(plan2.joint_trajectory.joint_names)
+  # Get the number of points on the trajectory
+  n_points1 = len(plan1.joint_trajectory.points)
+  n_points2 = len(plan2.joint_trajectory.points)
+  # Store the trajectory points
+  points1 = list(plan1.joint_trajectory.points)
+  points2 = list(plan2.joint_trajectory.points)
+  end_time = plan1.joint_trajectory.points[n_points1-1].time_from_start
+  start_time =  plan1.joint_trajectory.points[0].time_from_start
+  duration =  end_time - start_time
+  # add a time toleracne between  successive plans
+  time_tolerance = rospy.Duration.from_sec(0.1)
     
 
-    for i in range(n_points1):
-        point = JointTrajectoryPoint()
-        point.time_from_start = plan1.joint_trajectory.points[i].time_from_start
-        point.velocities = list(plan1.joint_trajectory.points[i].velocities)
-        point.accelerations = list(plan1.joint_trajectory.points[i].accelerations)
-        point.positions = plan1.joint_trajectory.points[i].positions
-        points1[i] = point
-        traj_msg.points.append(point)
-        end_time = plan1.joint_trajectory.points[i].time_from_start
+  for i in range(n_points1):
+    point = JointTrajectoryPoint()
+    point.time_from_start = plan1.joint_trajectory.points[i].time_from_start
+    point.velocities = list(plan1.joint_trajectory.points[i].velocities)
+    point.accelerations = list(plan1.joint_trajectory.points[i].accelerations)
+    point.positions = plan1.joint_trajectory.points[i].positions
+    points1[i] = point
+    traj_msg.points.append(point)
+    end_time = plan1.joint_trajectory.points[i].time_from_start
         
-    for i in range(n_points2):
-        point = JointTrajectoryPoint()
-        point.time_from_start = plan2.joint_trajectory.points[i].time_from_start + end_time +time_tolerance
-        point.velocities = list(plan2.joint_trajectory.points[i].velocities)
-        point.accelerations = list(plan2.joint_trajectory.points[i].accelerations)
-        point.positions = plan2.joint_trajectory.points[i].positions
-        #points1[i] = point
-        traj_msg.points.append(point)
+  for i in range(n_points2):
+    point = JointTrajectoryPoint()
+    point.time_from_start = plan2.joint_trajectory.points[i].time_from_start + end_time +time_tolerance
+    point.velocities = list(plan2.joint_trajectory.points[i].velocities)
+    point.accelerations = list(plan2.joint_trajectory.points[i].accelerations)
+    point.positions = plan2.joint_trajectory.points[i].positions
+    traj_msg.points.append(point)
     
     traj_msg.joint_names = plan1.joint_trajectory.joint_names
     traj_msg.header.frame_id = plan1.joint_trajectory.header.frame_id
