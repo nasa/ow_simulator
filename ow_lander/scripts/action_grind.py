c1ebcdd2eb39102a90954423cae6e814e003aaa0
anjan1984 <anjan1984@gmail.com>
2021-04-20 10:04:54 -0700

    code cleanup

--- ow_lander/scripts/action_grind.py
+++ ow_lander/scripts/action_grind.py
@@ -20,62 +20,11 @@ from action_deliver_sample import cascade_plans
 from action_dig_linear import go_to_Z_coordinate, change_joint_value
 from geometry_msgs.msg import Quaternion, PoseStamped, Pose
 from std_msgs.msg import Header
-
 from urdf_parser_py.urdf import URDF
-
 import kdl_parser_py.urdf
-#import sys
-#from pykdl_utils.kdl_kinematics import KDLKinematics
 
 
 grind_traj = RobotTrajectory()
-
-#def cascade_plans (plan1, plan2):
-  ## Create a new trajectory object
-  #new_traj = RobotTrajectory()
-  ## Initialize the new trajectory to be the same as the planned trajectory
-  #traj_msg = JointTrajectory()
-  ## Get the number of joints involved
-  #n_joints1 = len(plan1.joint_trajectory.joint_names)
-  #n_joints2 = len(plan2.joint_trajectory.joint_names)
-  ## Get the number of points on the trajectory
-  #n_points1 = len(plan1.joint_trajectory.points)
-  #n_points2 = len(plan2.joint_trajectory.points)
-  #print ('****************')
-  #print (n_points2 )
-  #print ('-----------------')
-  ## Store the trajectory points
-  #points1 = list(plan1.joint_trajectory.points)
-  #points2 = list(plan2.joint_trajectory.points)
-  #end_time = plan1.joint_trajectory.points[n_points1-1].time_from_start
-  #start_time =  plan1.joint_trajectory.points[0].time_from_start
-  #duration =  end_time - start_time
-  ## add a time toleracne between  successive plans
-  #time_tolerance = rospy.Duration.from_sec(0.1)
-    
-
-  #for i in range(n_points1):
-    #point = JointTrajectoryPoint()
-    #point.time_from_start = plan1.joint_trajectory.points[i].time_from_start
-    #point.velocities = list(plan1.joint_trajectory.points[i].velocities)
-    #point.accelerations = list(plan1.joint_trajectory.points[i].accelerations)
-    #point.positions = plan1.joint_trajectory.points[i].positions
-    #points1[i] = point
-    #traj_msg.points.append(point)
-    #end_time = plan1.joint_trajectory.points[i].time_from_start
-        
-  #for i in range(n_points2):
-    #point = JointTrajectoryPoint()
-    #point.time_from_start = plan2.joint_trajectory.points[i].time_from_start + end_time +time_tolerance
-    #point.velocities = list(plan2.joint_trajectory.points[i].velocities)
-    #point.accelerations = list(plan2.joint_trajectory.points[i].accelerations)
-    #point.positions = plan2.joint_trajectory.points[i].positions
-    #traj_msg.points.append(point)
-    
-  #traj_msg.joint_names = plan1.joint_trajectory.joint_names
-  #traj_msg.header.frame_id = plan1.joint_trajectory.header.frame_id
-  #new_traj.joint_trajectory = traj_msg
-  #return new_traj   
 
 def calculate_starting_state_grinder (plan,robot):
   #joint_names: [j_shou_yaw, j_shou_pitch, j_prox_pitch, j_dist_pitch, j_hand_yaw, j_grinder]
@@ -198,29 +147,22 @@ def grind(move_grinder, robot, moveit_fk, args):
 
   ## entering terrain
   z_start = ground_position + constants.GRINDER_OFFSET - depth
-  #cs, start_state  = calculate_starting_state_grinder (plan_a, robot)
   cs, start_state, goal_pose = calculate_joint_state_end_pose_from_plan_grinder (robot, plan_a, move_grinder, moveit_fk)
   plan_b = go_to_Z_coordinate(move_grinder, cs, goal_pose, x_start, y_start, z_start, False)
   
   grind_traj = cascade_plans (plan_a, plan_b)
   
   ## grinding ice forward
-  #cs, start_state = calculate_starting_state_grinder (plan_b, robot)
   cs, start_state, goal_pose = calculate_joint_state_end_pose_from_plan_grinder (robot, grind_traj, move_grinder, moveit_fk)
   cartesian_plan, fraction = plan_cartesian_path(move_grinder, goal_pose, length, alpha, parallel, z_start, cs)
   
   grind_traj = cascade_plans (grind_traj , cartesian_plan)
 
   ## grinding sideways
-  #cs, start_state  = calculate_starting_state_grinder (grind_traj, robot)
-  #joint_goal = move_grinder.get_current_joint_values()
   cs, start_state, joint_goal = calculate_joint_state_end_pose_from_plan_grinder (robot, grind_traj, move_grinder, moveit_fk)
   if parallel:
     plan_c = change_joint_value(move_grinder, cs, start_state, constants.J_SHOU_YAW, start_state[0]+0.08)
   else:
-    #x_now = 1.48266 + length*math.cos(alpha)
-    #y_now = -0.059 +length*math.sin(alpha)
-    #z_now = z_start
     x_now = joint_goal.position.x
     y_now = joint_goal.position.y
     z_now = joint_goal.position.z
@@ -231,14 +173,12 @@ def grind(move_grinder, robot, moveit_fk, args):
 
   grind_traj = cascade_plans (grind_traj , plan_c)
   ## grinding ice backwards
-  #cs, start_state  = calculate_starting_state_grinder (grind_traj, robot)
   cs, start_state, joint_goal = calculate_joint_state_end_pose_from_plan_grinder (robot, grind_traj, move_grinder, moveit_fk)
   cartesian_plan2, fraction2 = plan_cartesian_path(move_grinder, joint_goal, -length, alpha, parallel, z_start, cs)
   grind_traj = cascade_plans (grind_traj , cartesian_plan2)
 
 
   ## exiting terrain
-  #cs, start_state  = calculate_starting_state_grinder (grind_traj, robot)
   cs, start_state, joint_goal = calculate_joint_state_end_pose_from_plan_grinder (robot, grind_traj, move_grinder, moveit_fk)
   plan_d = go_to_Z_coordinate(move_grinder, cs, joint_goal, x_start, y_start, 0.22, False)
   grind_traj = cascade_plans (grind_traj , plan_d)
