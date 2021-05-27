ead9407655f77ac9881a0c51057765941d9e6eca
Terry Welsh <terence.m.welsh@nasa.gov>
2021-05-27 09:18:16 -0700

    Change python2 to python3.

--- ow_lander/scripts/stow_action_server.py
+++ ow_lander/scripts/stow_action_server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 # The Notices and Disclaimers for Ocean Worlds Autonomy Testbed for Exploration
 # Research and Simulation can be found in README.md in the root directory of
