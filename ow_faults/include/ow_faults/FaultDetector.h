ceaf0cfe1e40641cd5c65953c414ae7b2aeb50ff
lanseafood <lanssie.ma@nasa.gov>
2021-06-22 13:56:08 -0700

    new node setup

---
+++ ow_faults/include/ow_faults/FaultDetector.h
@@ -0,0 +1,50 @@
+// The Notices and Disclaimers for Ocean Worlds Autonomy Testbed for Exploration
+// Research and Simulation can be found in README.md in the root directory of
+// this repository.
+
+#ifndef FaultDetector_h
+#define FaultDetector_h
+
+#include <bitset>
+#include <ctime>
+#include <cstdlib>
+#include <ros/ros.h>
+#include <cstdint>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Empty.h>
+#include <ow_faults/FaultsConfig.h>
+#include "ow_faults/SystemFaults.h"
+#include "ow_faults/ArmFaults.h"
+#include "ow_faults/PowerFaults.h"
+#include "ow_faults/PTFaults.h"
+#include "ow_faults/CamFaults.h"
+// #include <ow_lander/lander_joints.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/WrenchStamped.h>
+#include <unordered_map>
+#include <random>
+
+
+// This class injects simple message faults that don't need to be simulated
+// at their source. Modified topics are prefixed with "/faults". This could be
+// accomplished on the original topics, but, for example, in the case of
+// /joint_states, this would require forking and modifying joint_state_publisher.
+// Creating a modified version of the original message is simpler, and it
+// leaves the original message intact in case we want to use it as part of a
+// placeholder fault estimator.
+class FaultDetector
+{
+
+public:
+  FaultDetector(ros::NodeHandle& node_handle);
+  ~FaultDetector(){}
+
+//   void faultsConfigCb(ow_faults::FaultsConfig& faults, uint32_t level);
+
+
+private:
+    ros::Publisher m_pub;
+
+};
+
+#endif
