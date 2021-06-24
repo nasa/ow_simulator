e501de0e9bd8d4b8b3dc2c0d092e5d70bbbd91d4
Ussama Naal <ussama.naal@nasa.gov>
2021-06-23 23:16:28 -0700

    unstow arm test fully abstracted

--- ow_sim_tests/test/arm_check.py
+++ ow_sim_tests/test/arm_check.py
@@ -20,7 +20,7 @@ class ArmCheck(unittest.TestCase):
 
   def __init__(self, *args, **kwargs):
     unittest.TestCase.__init__(self, *args, **kwargs)
-
+    rospy.init_node("arm_check_test", anonymous=True)
     self._test_duration = rospy.get_param("/arm_check/test_duration")
     moveit_commander.roscpp_initialize(sys.argv)
     self._robot = moveit_commander.RobotCommander()
@@ -47,39 +47,42 @@ class ArmCheck(unittest.TestCase):
   def _all_close(self, goal, actual, tolerance=0.01):
     """
     Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
-    @param: goal       A list of floats
-    @param: actual     A list of floats
-    @param: tolerance  A float
-    @returns: bool
+    @param goal: A list of floats
+    @param actual: A list of floats
+    @param tolerance: A float
+    @returns: True if all values satify the condition, otherwise False.
     """
     for index in range(len(goal)):
       if abs(actual[index] - goal[index]) > tolerance:
         return False
     return True
 
-  def _test_unstow(self):
-    rospy.init_node("test_unstow", anonymous=True)
-    rospy.wait_for_service('/arm/unstow')
+  def _test_activity(self,
+    service_name, service_type, joints_goal):
+    """
+    A helper method that does validates a any arm operation
+    @param service_name:
+    @param service_type:
+    @param joints_goal:
+    """
+
+    rospy.wait_for_service(service_name)
     success = False
     self._test_start_time = time.time()
 
     try:
-      arm_unstow = rospy.ServiceProxy('/arm/unstow', Unstow)
-      success = arm_unstow()
+      arm_activity = rospy.ServiceProxy(service_name, service_type)
+      success = arm_activity()
     except rospy.ServiceException as e:
-      rospy.logerr("arm/unstow threw an error: %s" % e)
+      rospy.logerr("{} threw an error: {}".format(service_name, e))
 
-    self.assertTrue(success)
+    self.assertTrue(success, "submitted request to " + service_name)
 
-    joints_target = self._arm_move_group.get_named_target_values("arm_unstowed")
     goal_state_achieved = False
-
-    joints_goal = self._map_named_joint_target_to_list(joints_target)
 
     while not rospy.is_shutdown() and \
         (time.time() - self._test_start_time < self._test_duration) and \
         not goal_state_achieved:
-      time.sleep(0.2)
 
       joints_state = self._arm_move_group.get_current_joint_values()
       rospy.loginfo("joints_goal = {}".format(joints_goal))
@@ -88,20 +91,15 @@ class ArmCheck(unittest.TestCase):
       rospy.loginfo("joints_state = {}".format(formatted_joints_state))
       goal_state_achieved = self._all_close(joints_goal, normalized_joints_state)
       rospy.loginfo("achieved? {}".format(goal_state_achieved))
+      time.sleep(0.2)
 
     self.assertTrue(goal_state_achieved)
 
   def test_unstow(self):
-    try:
-      self._test_unstow()
-    except Exception as e:
-      import traceback
-      rospy.logerr("{}".format(traceback.format_exc()))
-      rospy.logerr("exception: {}".format(e))
-    except:
-      rospy.logerr("Unexpected error: {}".format(sys.exc_info()[0]))
-      raise
-
+    joints_target = self._arm_move_group.get_named_target_values("arm_unstowed")
+    joints_goal = self._map_named_joint_target_to_list(joints_target)
+    self._test_activity(
+      service_name = '/arm/unstow', service_type = Unstow, joints_goal = joints_goal)
 
 if __name__ == '__main__':
   import rostest
