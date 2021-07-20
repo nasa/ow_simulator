3325d9e196cd2da598ce4ae13d881cef2d0dbb95
lanseafood <l.mao.ftwin@gmail.com>
2021-07-20 17:12:17 +0000

    removing duplicated

--- ow_faults_detection/include/ow_faults_detection/FaultDetector.h
+++ ow_faults_detection/include/ow_faults_detection/FaultDetector.h
@@ -72,74 +72,16 @@ public:
   static constexpr float THERMAL_MAX = 50;
   static constexpr float SOC_MIN = 0.1;
   static constexpr float SOC_MAX_DIFF = 0.05;
-
-  enum class ComponentFaults : uint {
-    Hardware = 1, 
-    JointLimit = 2,
-    TrajectoryGeneration = 2,
-    Collision = 3, 
-    Estop = 4, 
-    PositionLimit = 5, 
-    TorqueLimit = 6, 
-    VelocityLimit = 7, 
-    NoForceData = 8
-    };
-
-  //system
-  static constexpr std::bitset<10> isSystem{                0b00'0000'0001 };
-  static constexpr std::bitset<10> isArmGoalError{          0b00'0000'0010 };
-  static constexpr std::bitset<10> isArmExecutionError{     0b00'0000'0100 };
-  static constexpr std::bitset<10> isTaskGoalError{         0b00'0000'1000 };
-  static constexpr std::bitset<10> isCamGoalError{          0b00'0001'0000 };
-  static constexpr std::bitset<10> isCamExecutionError{     0b00'0010'0000 };
-  static constexpr std::bitset<10> isPanTiltGoalError{      0b00'0100'0000 };
-  static constexpr std::bitset<10> isPanTiltExecutionError{ 0b00'1000'0000 };
-  static constexpr std::bitset<10> isLanderExecutionError{  0b01'0000'0000 };
-  static constexpr std::bitset<10> isPowerSystemFault{      0b10'0000'0000 };
   
-  //power
-  static constexpr std::bitset<3> islowVoltageError{ 0b001 };
-  static constexpr std::bitset<3> isCapLossError{    0b010 };
-  static constexpr std::bitset<3> isThermalError{    0b100 };
-
-  static constexpr float THERMAL_MAX = 50;
-  static constexpr float SOC_MIN = 0.1;
-  static constexpr float SOC_MAX_DIFF = 0.05;
-
 private:
   //  ////////// functions
   void publishSystemFaultsMessage();
-
-  // //Arm functions
-  // // Output /faults/joint_states, a modified version of /joint_states, injecting
-  // // simple message faults that don't need to be simulated at their source.
-  // void jointStateCb(const sensor_msgs::JointStateConstPtr& msg);
-  // // Output /faults/joint_states, a modified version of /joint_states, injecting
-  // // simple message faults that don't need to be simulated at their source.
-  // void distPitchFtSensorCb(const geometry_msgs::WrenchStamped& msg);
-  // // Find an item in an std::vector or other find-able data structure, and
-  // // return its index. Return -1 if not found.
-  // template<typename group_t, typename item_t>
-  // int findPositionInGroup(const group_t& group, const item_t& item);
-  // // Get index from m_joint_index_map. If found, modify out_index and return
-  // // true. Otherwise, return false.
-  // bool findJointIndex(const unsigned int joint, unsigned int& out_index);
-
-  // //camera function
-  // void cameraTriggerOriginalCb(const std_msgs::Empty& msg);
-  // void cameraTriggerCb(const std_msgs::Empty& msg);
 
   // // power functions
   float getRandomFloatFromRange(float min_val, float max_val);
   void publishPowerSystemFault();
   void powerSOCListener(const std_msgs::Float64& msg);
   void powerTemperatureListener(const std_msgs::Float64& msg);
-
-  // // Antennae functions
-  // void antennaPanFaultCb(const std_msgs::Float64& msg);
-  // void antennaTiltFaultCb(const std_msgs::Float64& msg);
-  // void publishAntennaeFaults(const std_msgs::Float64& msg, bool encoder, 
-  //                            bool torque, float& m_faultValue, ros::Publisher& m_publisher);
 
   // //Setting message values
   template<typename fault_msg>
@@ -148,41 +90,14 @@ private:
   void setBitsetFaultsMessage(bitsetFaultsMsg& msg, bitmask systemFaultsBitmask);
   template<typename fault_msg>
   void setComponentFaultsMessage(fault_msg& msg, ComponentFaults value);
- 
-  // //checking rqt faults
-  // void checkArmFaults();
-  // void checkAntFaults();
-  // void checkCamFaults();
 
-  // ///////publishers and subscribers
-  // // arm faults
-  // ros::Subscriber m_joint_state_sub;
-  // ros::Publisher m_joint_state_pub;
 
   // //power
   ros::Subscriber m_power_soc_sub;
   ros::Subscriber m_power_temperature_sub;
   ros::Publisher m_power_fault_trigger_pub;
 
-  // // ft sensor
-  // ros::Subscriber m_dist_pitch_ft_sensor_sub;
-  // ros::Publisher m_dist_pitch_ft_sensor_pub;
-
-  // // camera
-  // ros::Subscriber m_camera_original_trigger_sub;
-  // ros::Subscriber m_camera_trigger_sub;
-  // ros::Publisher m_camera_trigger_remapped_pub;
-
-  // //antenna 
-  // ros::Subscriber m_fault_ant_pan_sub;
-  // ros::Subscriber m_fault_ant_tilt_sub;
-  // ros::Publisher m_fault_ant_pan_remapped_pub;
-  // ros::Publisher m_fault_ant_tilt_remapped_pub;
-
   // // jpl message publishers
-  // ros::Publisher m_antenna_fault_msg_pub;
-  // ros::Publisher m_arm_fault_msg_pub;
-  // ros::Publisher m_camera_fault_msg_pub;
   ros::Publisher m_power_fault_msg_pub;
   ros::Publisher m_system_fault_msg_pub;
 
@@ -191,21 +106,12 @@ private:
   std::bitset<10> m_system_faults_bitset{};
 
   // //general component faults
-  // bool m_arm_fault;
-  // bool m_ant_fault;
-  // bool m_cam_trigger_on = false;
   bool m_soc_fault = false;
   bool m_temperature_fault = false;
-
-  // //arm joint faults
-  // ow_faults::FaultsConfig m_faults;
 
   // //power vars
   float m_last_SOC = std::numeric_limits<float>::quiet_NaN();
 
-  // // antenna vars
-  // float m_fault_pan_value;
-  // float m_fault_tilt_value;
 };
 
 #endif
