493a1a5a10eeb66cf14151f028d33f1e5c76b4df
lanseafood <l.mao.ftwin@gmail.com>
2021-07-20 17:33:14 +0000

    cleanup injector

--- ow_faults/include/ow_faults/FaultInjector.h
+++ ow_faults/include/ow_faults/FaultInjector.h
@@ -64,15 +64,6 @@ public:
   static constexpr std::bitset<10> isPanTiltExecutionError{ 0b00'1000'0000 };
   static constexpr std::bitset<10> isLanderExecutionError{  0b01'0000'0000 };
   static constexpr std::bitset<10> isPowerSystemFault{      0b10'0000'0000 };
-  
-  //power
-  static constexpr std::bitset<3> islowVoltageError{ 0b001 };
-  static constexpr std::bitset<3> isCapLossError{    0b010 };
-  static constexpr std::bitset<3> isThermalError{    0b100 };
-
-  static constexpr float THERMAL_MAX = 50;
-  static constexpr float SOC_MIN = 0.1;
-  static constexpr float SOC_MAX_DIFF = 0.05;
 
   //arm
   static constexpr float FAULT_ZERO_TELEMETRY = 0.0;
@@ -100,26 +91,12 @@ private:
   //camera function
   void cameraTriggerCb(const std_msgs::Empty& msg);
 
-  // power functions
-  float getRandomFloatFromRange(float min_val, float max_val);
-  void publishPowerSystemFault();
-  void powerSOCListener(const std_msgs::Float64& msg);
-  void powerTemperatureListener(const std_msgs::Float64& msg);
-
   // Antennae functions
   void antennaPanFaultCb(const std_msgs::Float64& msg);
   void antennaTiltFaultCb(const std_msgs::Float64& msg);
   void publishAntennaeFaults(const std_msgs::Float64& msg, bool encoder, 
                              bool torque, float& m_faultValue, ros::Publisher& m_publisher);
 
-  //Setting message values
-  template<typename fault_msg>
-  void setFaultsMessageHeader(fault_msg& msg);
-  template<typename bitsetFaultsMsg, typename bitmask>
-  void setBitsetFaultsMessage(bitsetFaultsMsg& msg, bitmask systemFaultsBitmask);
-  template<typename fault_msg>
-  void setComponentFaultsMessage(fault_msg& msg, ComponentFaults value);
- 
   //checking rqt faults
   void checkArmFaults();
   void checkAntFaults();
@@ -129,11 +106,6 @@ private:
   // arm faults
   ros::Subscriber m_joint_state_sub;
   ros::Publisher m_joint_state_pub;
-
-  //power
-  ros::Subscriber m_power_soc_sub;
-  ros::Subscriber m_power_temperature_sub;
-  ros::Publisher m_power_fault_trigger_pub;
 
   // ft sensor
   ros::Subscriber m_dist_pitch_ft_sensor_sub;
@@ -153,8 +125,6 @@ private:
   ros::Publisher m_antenna_fault_msg_pub;
   ros::Publisher m_arm_fault_msg_pub;
   ros::Publisher m_camera_fault_msg_pub;
-  ros::Publisher m_power_fault_msg_pub;
-  ros::Publisher m_system_fault_msg_pub;
 
   ////////// vars
   //system
@@ -164,14 +134,9 @@ private:
   bool m_arm_fault;
   bool m_ant_fault;
   bool m_cam_fault = false;
-  bool m_soc_fault = false;
-  bool m_temperature_fault = false;
 
   //arm joint faults
   ow_faults::FaultsConfig m_faults;
-
-  //power vars
-  float m_last_SOC = std::numeric_limits<float>::quiet_NaN();
 
   // antenna vars
   float m_fault_pan_value;
--- ow_faults/src/FaultInjector.cpp
+++ ow_faults/src/FaultInjector.cpp
@@ -13,10 +13,6 @@ constexpr std::bitset<10> FaultInjector::isPanTiltExecutionError;
 constexpr std::bitset<10> FaultInjector::isArmExecutionError;
 constexpr std::bitset<10> FaultInjector::isPowerSystemFault;
 
-constexpr std::bitset<3> FaultInjector::islowVoltageError;
-constexpr std::bitset<3> FaultInjector::isCapLossError;
-constexpr std::bitset<3> FaultInjector::isThermalError;
-
 FaultInjector::FaultInjector(ros::NodeHandle& node_handle)
 {
   //  arm pub and subs
@@ -42,18 +38,7 @@ FaultInjector::FaultInjector(ros::NodeHandle& node_handle)
   // m_antenna_fault_msg_pub = node_handle.advertise<ow_faults::PTFaults>("/faults/pt_faults_status", 10);
   // m_arm_fault_msg_pub = node_handle.advertise<ow_faults::ArmFaults>("/faults/arm_faults_status", 10);
   // m_camera_fault_msg_pub = node_handle.advertise<ow_faults::CamFaults>("/faults/cam_faults_status", 10);
-  // m_power_fault_msg_pub = node_handle.advertise<ow_faults::PowerFaults>("/faults/power_faults_status", 10);
   // m_system_fault_msg_pub = node_handle.advertise<ow_faults::SystemFaults>("/faults/system_faults_status", 10);
-
-  //power fault publishers and subs
-  // m_power_soc_sub = node_handle.subscribe("/power_system_node/state_of_charge",
-  //                                         10,
-  //                                         &FaultInjector::powerSOCListener,
-  //                                         this);
-  // m_power_temperature_sub = node_handle.subscribe("/power_system_node/battery_temperature",
-  //                                                 10,
-  //                                                 &FaultInjector::powerTemperatureListener,
-  //                                                 this);
 
   //antenna fault publishers and subs
   m_fault_ant_pan_sub = node_handle.subscribe("/_original/ant_pan_position_controller/command",
@@ -76,25 +61,6 @@ void FaultInjector::faultsConfigCb(ow_faults::FaultsConfig& faults, uint32_t lev
   // Store current set of faults for later use
   m_faults = faults;
 }
-
-// Creating Fault Messages
-template<typename fault_msg>
-void FaultInjector::setFaultsMessageHeader(fault_msg& msg){
-  msg.header.stamp = ros::Time::now();
-  msg.header.frame_id = "world";
-}
-
-template<typename bitsetFaultsMsg, typename bitmask>
-void FaultInjector::setBitsetFaultsMessage(bitsetFaultsMsg& msg, bitmask bm) {
-  setFaultsMessageHeader(msg);
-  msg.value = bm.to_ullong();
-}
-
-// template<typename fault_msg>
-// void FaultInjector::setComponentFaultsMessage(fault_msg& msg, ComponentFaults value) {
-//   setFaultsMessageHeader(msg);
-//   msg.value = static_cast<uint>(value);
-// }
 
 void FaultInjector::cameraTriggerCb(const std_msgs::Empty& msg){
   if (!m_cam_fault) {// if no fault
@@ -128,44 +94,6 @@ void FaultInjector::antennaTiltFaultCb(const std_msgs::Float64& msg){
                         m_faults.ant_tilt_effort_failure,
                         m_fault_tilt_value, m_fault_ant_tilt_remapped_pub );
 }
-
-// float FaultInjector::getRandomFloatFromRange( float min_val, float max_val){
-//   return min_val + (max_val - min_val) * (rand() / static_cast<float>(RAND_MAX));
-// }
-
-// void FaultInjector::publishPowerSystemFault(){
-//   ow_faults::PowerFaults power_faults_msg;
-//   //update if fault
-//   if (m_temperature_fault || m_soc_fault) {
-//     //system
-//     m_system_faults_bitset |= isPowerSystemFault;
-//     //power
-//     setComponentFaultsMessage(power_faults_msg, ComponentFaults::Hardware);
-//   } else {
-//     m_system_faults_bitset &= ~isPowerSystemFault;
-//   }
-//   // publishSystemFaultsMessage();
-//   // m_power_fault_msg_pub.publish(power_faults_msg);
-// }
-
-// void FaultInjector::powerTemperatureListener(const std_msgs::Float64& msg)
-// {
-//   m_temperature_fault = msg.data > THERMAL_MAX;
-//   publishPowerSystemFault();
-// }
-
-// void FaultInjector::powerSOCListener(const std_msgs::Float64& msg)
-// {
-//   float newSOC = msg.data;
-//   if (isnan(m_last_SOC)){
-//     m_last_SOC = newSOC;
-//   }
-//   m_soc_fault = ((newSOC <= SOC_MIN)  ||
-//         (!isnan(m_last_SOC) &&
-//         ((abs(m_last_SOC - newSOC) / m_last_SOC) >= SOC_MAX_DIFF )));
-//   publishPowerSystemFault();
-//   m_last_SOC = newSOC;
-// }
 
 void FaultInjector::jointStateCb(const sensor_msgs::JointStateConstPtr& msg)
 {
@@ -280,12 +208,6 @@ void FaultInjector::jointStateCb(const sensor_msgs::JointStateConstPtr& msg)
   // m_antenna_fault_msg_pub.publish(pt_faults_msg);
   // m_camera_fault_msg_pub.publish(camera_faults_msg);
 }
-
-// void FaultInjector::publishSystemFaultsMessage(){
-//   ow_faults::SystemFaults system_faults_msg;
-//   setBitsetFaultsMessage(system_faults_msg, m_system_faults_bitset);
-//   m_system_fault_msg_pub.publish(system_faults_msg);
-// }
 
 void FaultInjector::distPitchFtSensorCb(const geometry_msgs::WrenchStamped& msg)
 {
