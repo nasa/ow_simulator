b2db76b82446eb8ec89e6387e39e4bf03d420d13
anjan1984 <anjan1984@gmail.com>
2021-08-26 16:25:30 -0700

    guarded move updated

--- ow_lander/scripts/guarded_move_action_server.py
+++ ow_lander/scripts/guarded_move_action_server.py
@@ -8,7 +8,8 @@ import rospy
 import actionlib
 import ow_lander.msg
 import sys
-import action_guarded_move
+#import action_guarded_move
+import all_action_trajectories
 from LanderInterface import MoveItInterface
 from LanderInterface import LinkStateSubscriber
 from trajectory_async_execution import TrajectoryAsyncExecuter
@@ -84,7 +85,7 @@ class GuardedMoveActionServer(object):
         
     def _update_motion(self, goal):
         print("Guarded move activity started")
-        self.guarded_move_traj, self._guarded_move_plan_ratio = action_guarded_move.guarded_move_plan(
+        self.guarded_move_traj, self._guarded_move_plan_ratio = all_action_trajectoris.guarded_move_plan(
                                               self._interface.move_arm,
                                               self._interface.robot, 
                                               self._interface.moveit_fk, goal)
