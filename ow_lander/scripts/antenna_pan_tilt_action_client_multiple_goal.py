bb78c20df8e3c9553312b992fbc2134781b449f1
anjan1984 <anjan1984@gmail.com>
2021-10-26 16:40:49 -0700

    multiple goals

--- ow_lander/scripts/antenna_pan_tilt_action_client_multiple_goal.py
+++ ow_lander/scripts/antenna_pan_tilt_action_client_multiple_goal.py
@@ -8,6 +8,7 @@ import rospy
 import actionlib
 from math import pi
 import ow_lander.msg
+from actionlib_msgs.msg import *
 
 def wrap_angle(angle):
     """
@@ -25,9 +26,6 @@ class antenna_client_multiple_goals():
     
     def __init__(self):
         rospy.init_node('antenna_client_multiple_goal', anonymous = False)
-        
-        rospy.on_shutdown(self.shutdown)
- 
         self.client = actionlib.SimpleActionClient('AntennaPanTiltAction', ow_lander.msg.AntennaPanTiltAction)
         rospy.loginfo("Waiting for AntennaPanTilt action server...")
         self.client.wait_for_server()
@@ -35,22 +33,25 @@ class antenna_client_multiple_goals():
         rospy.loginfo("Starting AntennaPanTilt action")
 
         goal = ow_lander.msg.AntennaPanTiltGoal()
+        
+        pan_values = [ 0.0 , 0.5, 1.0, 1.5]
+        tilt_values = [ 0.0 , 0.5, 1.0, 1.5]
+ 
+        for i in range(len(pan_values)):
 
-        goal.pan = 0.5
-        goal.tilt = 0.0
-        goal.tilt = wrap_angle(goal.tilt)
-        goal.pan = wrap_angle(goal.pan)
-        
-        
-        self.move(goal)
+            goal.pan = pan_values[i]
+            goal.tilt = tilt_values[i]
+            goal.tilt = wrap_angle(goal.tilt)
+            goal.pan = wrap_angle(goal.pan)       
+            self.move(goal)
         
     def move(self, goal):    
 
         # Sends the goal to the action server.
         self.client.send_goal(goal)
 
-        # Waits for the server to finish performing the action.
-        success = self.client.wait_for_result()
+        # Waits for the server to finish performing the action. alloting 30 secs
+        success = self.client.wait_for_result(rospy.Duration(30))
         
         # If we don't get there in time, abort the goal
         if not success:
@@ -60,20 +61,10 @@ class antenna_client_multiple_goals():
             # We made it!
             state = self.client.get_state()
             result = self.client.get_result()
-            if state == 3:
+            if state == GoalStatus.SUCCEEDED:
                 rospy.loginfo("Goal succeeded!")
                 # Prints out the result of executing the action
                 rospy.loginfo("Result: %s", result)
-
-        
-    def shutdown(self):
-        rospy.loginfo("Stopping the robot...")
-        # Cancel any active goals
-        self.client.cancel_goal()
-        rospy.sleep(2)
-        # Stop the robot
-        #self.cmd_vel_pub.publish(Twist())
-        #rospy.sleep(1)    
 
 if __name__ == '__main__':
     try:
