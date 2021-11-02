10769f88237732a05a95333bf1b99e2a2ae2aa31
anjan1984 <anjan1984@gmail.com>
2021-11-02 16:53:28 -0700

    deliver sample with arguments

--- ow_lander/scripts/deliver_action_client.py
+++ ow_lander/scripts/deliver_action_client.py
@@ -7,18 +7,36 @@
 import rospy
 import actionlib
 import ow_lander.msg
+import argparse
 
 def deliver_client():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('x_start', type=float,
+                        help='x coordinates of sample delivery location', nargs='?', default=0.55, const=0)
+    parser.add_argument('y_start', type=float,
+                        help='y coordinates of sample delivery location', nargs='?', default=-0.3, const=0)
+    parser.add_argument('z_start', type=float,
+                        help='z coordinates of sample delivery location', nargs='?', default=0.82, const=0)
+    args = parser.parse_args()
+    rospy.loginfo("Requested x_start: %s", args.x_start)
+    rospy.loginfo("Requested y_start: %s", args.y_start)
+    rospy.loginfo("Requested z_start: %s", args.z_start)
+
  
     client = actionlib.SimpleActionClient('Deliver', ow_lander.msg.DeliverAction)
 
     client.wait_for_server()
 
     goal = ow_lander.msg.DeliverGoal()
+    
+    goal.delivery.x = args.x_start
+    goal.delivery.y = args.y_start
+    goal.delivery.z = args.z_start
     # Default trenching values
-    goal.delivery.x = 0.55
-    goal.delivery.y = -0.3
-    goal.delivery.z = 0.82 
+    # goal.delivery.x = 0.55
+    # goal.delivery.y = -0.3
+    # goal.delivery.z = 0.82 
     
     # Remove Tailings values
     #goal.delivery.x = 1.5
