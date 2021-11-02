6f9f8aac05c60470e40dab31688c486e523e11ef
anjan1984 <anjan1984@gmail.com>
2021-11-02 12:13:38 -0700

    guarded move with arguments added

--- ow_lander/scripts/guarded_move_action_client.py
+++ ow_lander/scripts/guarded_move_action_client.py
@@ -8,23 +8,38 @@ import rospy
 import actionlib
 import ow_lander.msg
 import sys
+import argparse
 
 def guarded_move_client():
- 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('x_start', type=float, help='x coordinates of guarded move starting point', nargs='?', default=2.0, const=0)
+    parser.add_argument('y_start', type=float, help='y coordinates of guarded move starting point', nargs='?', default=0.0, const=0)
+    parser.add_argument('z_start', type=float, help='z coordinates of guarded move starting point', nargs='?', default=0.3, const=0)
+    parser.add_argument('direction_x', type=float, help='x coordinates of the vector normal to the surface', nargs='?', default=0.0, const=0)
+    parser.add_argument('direction_y', type=float, help='y coordinates of the vector normal to the surface', nargs='?', default=0.0, const=0)
+    parser.add_argument('direction_z', type=float, help='z coordinates of the vector normal to the surface', nargs='?', default=1.0, const=0)
+    parser.add_argument('search_distance', type=float, help='Search Distance', nargs='?', default=0.5, const= 0 )
+    args = parser.parse_args()
+    rospy.loginfo("Requetsed x_start: %s", args.x_start)
+    rospy.loginfo("Requetsed y_start: %s", args.y_start)
+    rospy.loginfo("Requetsed z_start: %s", args.z_start)
+    rospy.loginfo("Requetsed normal.x: %s", args.direction_x)
+    rospy.loginfo("Requetsed normal.y: %s", args.direction_y)
+    rospy.loginfo("Requetsed normal.z: %s", args.direction_z)
+    rospy.loginfo("Requetsed search distance: %s", args.search_distance)
     client = actionlib.SimpleActionClient('GuardedMove', ow_lander.msg.GuardedMoveAction)
 
     client.wait_for_server()
 
     goal = ow_lander.msg.GuardedMoveGoal()
     
-    # Default trenching values
-    goal.start.x = 2.0
-    goal.start.y = 0.0
-    goal.start.z = 0.3
-    goal.normal.x = 0.0
-    goal.normal.y = 0.0
-    goal.normal.z = 1.0
-    goal.search_distance = 0.5
+    goal.start.x = args.x_start
+    goal.start.y = args.y_start
+    goal.start.z = args.z_start
+    goal.normal.x = args.direction_x
+    goal.normal.y = args.direction_y
+    goal.normal.z = args.direction_z
+    goal.search_distance = args.search_distance
 
     # Sends the goal to the action server.
     client.send_goal(goal)
