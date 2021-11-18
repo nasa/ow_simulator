3a22af44e95df4f569cf1f4601a947bce35e6ad8
Thomas Stucky <thomas@astrostucky.com>
2021-11-18 15:14:10 -0700

    Fixed arm_check test calling deliver with parameters  - arm_check now normalizes its joint goals before comparing with joint states  - arm_check uses arm_deliver_final as the goal for the deliver test

--- ow_sim_tests/test/arm_check.py
+++ ow_sim_tests/test/arm_check.py
@@ -38,7 +38,7 @@ class ArmCheck(unittest.TestCase):
 
   def _normalize_angle(self, angle):
     """
-    Convenience method to nomrlaize angles to the range [-pi, pi]
+    Convenience method to normalize angles to the range [-pi, pi]
     @param angle: An angle that might need to be normalized
     @returns: the normalized value of the angle
     """
@@ -66,19 +66,20 @@ class ArmCheck(unittest.TestCase):
 
     rospy.wait_for_service(service_name)
     test_start_time = rospy.get_time()
-    joints_goal_array = np.array(joints_goal)
+    norm_joints_goal_array = np.array(
+      [self._normalize_angle(e) for e in joints_goal])
     rospy.loginfo("""
     test info:
     ----------
     service name: {}
     joints goal: {}
     start time: {}""".format(
-      service_name, np.round(joints_goal_array, 2), test_start_time))
+      service_name, np.round(norm_joints_goal_array, 2), test_start_time))
     arm_activity = rospy.ServiceProxy(service_name, service_type)
     success = arm_activity() if service_args is None else arm_activity(*service_args)
     self.assertTrue(success, "submitted request to " + service_name)
     goal_state_achieved = False
-    joints_abs_diff_array = np.full(joints_goal_array.shape, np.inf)
+    joints_abs_diff_array = np.full(norm_joints_goal_array.shape, np.inf)
 
     elapsed = 0
     while not rospy.is_shutdown() and \
@@ -89,7 +90,7 @@ class ArmCheck(unittest.TestCase):
       norm_joints_state_array = np.array(
           [self._normalize_angle(e) for e in joints_state])
       joints_abs_diff_array = np.abs(
-          joints_goal_array - norm_joints_state_array)
+          norm_joints_goal_array - norm_joints_state_array)
       goal_state_achieved = all(joints_abs_diff_array < tolerance)
       rospy.loginfo("""
       current joints state: {}
@@ -175,12 +176,14 @@ class ArmCheck(unittest.TestCase):
         service_args=[True, 0, 0, 0, 0, 0])
 
   def test_08_deliver_sample(self):
+    joints_target = self._arm_move_group.get_named_target_values(
+        "arm_deliver_final")
+    joints_goal = self._map_named_joint_target_to_list(joints_target)
     self._test_activity(
         service_name='/arm/deliver_sample',
         service_type=DeliverSample,
-        joints_goal=[0.98, 1.78, 0.96, 1.98, 2.55, 1.58],
-        test_duration=90,
-        service_args=[True, 0, 0, 0])
+        joints_goal=joints_goal,
+        test_duration=90)
 
   def test_09_unstow(self):
     joints_target = self._arm_move_group.get_named_target_values(
