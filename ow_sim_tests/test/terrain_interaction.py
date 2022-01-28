39d2da35a989c304abfd87162511b5ef9ac53a07
Thomas Stucky <thomas@astrostucky.com>
2022-01-28 14:37:48 -0700

    Grind portion of test now works. Added more asserts and logging.

--- ow_sim_tests/test/terrain_interaction.py
+++ ow_sim_tests/test/terrain_interaction.py
@@ -17,7 +17,9 @@ import ow_lander.msg
 PKG = 'ow_sim_tests'
 roslib.load_manifest(PKG)
 
-ACTION_SERVER_WAIT_TIMEOUT = rospy.Duration(5.0)
+ACTION_SERVER_WAIT_TIMEOUT = rospy.Duration(10.0)
+
+ACCEPTABLE_POSE_POINT_VARIANCE = 0.005
 
 class TerrainInteraction(unittest.TestCase):
 
@@ -38,15 +40,22 @@ class TerrainInteraction(unittest.TestCase):
   """
   def _start_action(self, action_name, action, goal, timeout, **kwargs):
     client = actionlib.SimpleActionClient(action_name, action)
-    client.wait_for_server(timeout=timeout)
+    connected = client.wait_for_server(timeout=timeout)
+    self.assertTrue(connected, "Timeout exceeded waiting for %s action server" % action_name)
+    client.wait_for_server()
 
-    # use kwargs here
+    # populate goal parameters using kwargs
+    parameter_list = ""
     for key, value in kwargs.items():
       if hasattr(goal, key):
         setattr(goal, key, value)
+        parameter_list += "\n%s : %s" % (key, str(value))
       else:
         self.fail("%s action does not contain parameter %s" % (action_name, key))
+
     client.send_goal(goal)
+    rospy.loginfo("\n===%s Action Goal Parameters Sent===%s" % (action_name, parameter_list))
+
     return client
 
   """
@@ -54,15 +63,19 @@ class TerrainInteraction(unittest.TestCase):
   @param action_client: ROS action client object
   """
   def _is_action_done(self, action_client):
-    rospy.loginfo("state = %d" % action_client.simple_state)
     return action_client.simple_state == actionlib.SimpleGoalState.DONE
+
+  def assert_point_almost_equals(self, p1, p2, delta=ACCEPTABLE_POSE_POINT_VARIANCE):
+    self.assertAlmostEqual(p1.x, p2.x, delta=delta)
+    self.assertAlmostEqual(p1.y, p2.y, delta=delta)
+    self.assertAlmostEqual(p1.z, p2.z, delta=delta)
 
   def test_01_grind(self):
 
     GRIND_ACTIVITY_TIMEOUT = 60.0
 
     grind_client = self._start_action(
-      "Grind",
+      'Grind',
       ow_lander.msg.GrindAction,
       ow_lander.msg.GrindGoal(),
       ACTION_SERVER_WAIT_TIMEOUT,
@@ -80,7 +93,9 @@ class TerrainInteraction(unittest.TestCase):
     """
     condition_violated = False
     def condition_test(message):
+      nonlocal condition_violated
       condition_violated = "regolith_0" in message.name
+
     subscriber = rospy.Subscriber("/gazebo/model_states", ModelStates, condition_test)
     start_time = rospy.get_time()
     elapsed = 0
@@ -91,6 +106,7 @@ class TerrainInteraction(unittest.TestCase):
       rospy.sleep(0.2)
       elapsed = rospy.get_time() - start_time
     subscriber.unregister()
+    self.assertTrue(elapsed < GRIND_ACTIVITY_TIMEOUT, "Timeout reached waiting for Grind action to finish!")
     self.assertFalse(condition_violated, "Regolith model spawned during the grind operation!")
 
     """
@@ -99,9 +115,10 @@ class TerrainInteraction(unittest.TestCase):
       2. Terrain has changed shape
     """
     grind_result = grind_client.get_result()
-    # TODO: test for final value
-    # self.assertEquals(grind_result.final, Point())
-    # TODO: find way to check if terrain has changed
+    expected_final = Point(1.4715559423041809, -0.14057547008097895, -6.739779746454913)
+    self.assert_point_almost_equals(grind_result.final, expected_final)
+
+    # TODO: find way to check if collision terrain has changed
 
 if __name__ == '__main__':
   import rostest
