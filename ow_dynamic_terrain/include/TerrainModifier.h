658272de68009e6b928d523428989f38ac23e371
Thomas Stucky <thomas.stucky@nasa.gov>
2022-04-13 17:54:50 -0600

    Messages reverted back to using Point32

--- ow_dynamic_terrain/include/TerrainModifier.h
+++ ow_dynamic_terrain/include/TerrainModifier.h
@@ -6,7 +6,7 @@
 #define TERRAIN_MODIFIER_H
 
 #include <cv_bridge/cv_bridge.h>
-#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Point32.h>
 #include <gazebo/rendering/Heightmap.hh>
 #include "ow_dynamic_terrain/modify_terrain_circle.h"
 #include "ow_dynamic_terrain/modify_terrain_ellipse.h"
@@ -41,7 +41,7 @@ private:
   // param heightmap: reference heightmap used for the conversion.
   // param position: a position in world coordinates
   static cv::Point2i getHeightmapPosition(gazebo::rendering::Heightmap* heightmap,
-                                          const geometry_msgs::Point& position);
+                                          const geometry_msgs::Point32& position);
 
   // Imports an OpenCV Matrix object from a sensor_msgs::Image object through cv_bridge
   static cv_bridge::CvImageConstPtr importImageToOpenCV(const ow_dynamic_terrain::modify_terrain_patch::ConstPtr& msg);
