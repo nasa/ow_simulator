334ac6d85b68452d2d177ac4ee460830169b4e41
Terry Welsh <terence.m.welsh@nasa.gov>
2022-07-19 10:24:44 -0700

    Apply modified normal map to terrain.

--- ow_dynamic_terrain/src/DynamicTerrainVisual.cpp
+++ ow_dynamic_terrain/src/DynamicTerrainVisual.cpp
@@ -4,10 +4,12 @@
 
 #include "DynamicTerrainBase.h"
 #include "TerrainModifier.h"
+#include <gazebo/rendering/RenderingIface.hh>
 
 using namespace std;
 using namespace gazebo;
 using namespace rendering;
+using namespace Ogre;
 
 namespace ow_dynamic_terrain
 {
@@ -61,6 +63,41 @@ private:
       terrain->updateDerivedData(false, Ogre::Terrain::DERIVED_DATA_NORMALS | Ogre::Terrain::DERIVED_DATA_LIGHTMAP);
 
       m_differential_pub.publish(diff_msg);
+
+      // Override custom material normal map labeled "ow_dynamic_terrain_normal_map"
+      // with Ogre::Terrain's internal normal map.
+      // Ogre::Terrain makes a copy of a user-specified normal map and the copy
+      // is the one that is updated by Terrain::updateDerivedData(), not the
+      // original normal map specified in a custom material file.
+      // Note: If you are not using a custom material this code will have no
+      // effect, but Ogre should be using the correct normal map and a generated
+      // material and shader in that case.
+      // Caveat: The normal map copy is always resized to the terrain's DEM
+      // resolution, so using a larger texture for more detail is not possible
+      // with this plugin. It might be possible to update the original normal map
+      // by computing all necessary heights at the original resolution and then
+      // computing and uploading normals instead of relying on Ogre to do it. 
+      ResourceManager::ResourceMapIterator res_it = MaterialManager::getSingleton().getResourceIterator();
+      while (res_it.hasMoreElements())
+      {
+        ResourcePtr resource = res_it.getNext();
+        MaterialPtr material = resource.staticCast<Material>();
+        Material::TechniqueIterator tech_it = material->getTechniqueIterator();
+        while (tech_it.hasMoreElements())
+        {
+          Technique* technique = tech_it.getNext();
+          Technique::PassIterator pass_it = technique->getPassIterator();
+          while (pass_it.hasMoreElements())
+          {
+            Pass* pass = pass_it.getNext();
+            TextureUnitState* tus = pass->getTextureUnitState("ow_dynamic_terrain_normal_map");
+            if (tus != 0)
+            {
+              tus->setTexture(terrain->getTerrainNormalMap());
+            }
+          }
+        }
+      }
     }
   }
 
