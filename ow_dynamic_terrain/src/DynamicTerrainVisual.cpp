7e7857ce3147a52b04e38b6bdeddd6208a6e22db
Terry Welsh <terence.m.welsh@nasa.gov>
2022-07-26 12:22:47 -0700

    Apply normal map only once upon first render. Map's contents will be updated later by Terrain::updateDerivedData().

--- ow_dynamic_terrain/src/DynamicTerrainVisual.cpp
+++ ow_dynamic_terrain/src/DynamicTerrainVisual.cpp
@@ -23,6 +23,13 @@ public:
   void Load(VisualPtr /*visual*/, sdf::ElementPtr /*sdf*/) override
   {
     Initialize("visual");
+
+    m_on_prerender_connection = gazebo::event::Events::ConnectPreRender([this]() {
+      // Initialize terrain with Ogre-generated normal map.
+      applyNormalMap();
+      // Disconnect so this method will not be called again.
+      m_on_prerender_connection.reset();
+    });
   }
 
 private:
@@ -45,7 +52,7 @@ private:
     auto heightmap = getHeightmap(get_scene());
     if (heightmap == nullptr)
     {
-      gzerr << m_plugin_name << ": Couldn't acquire heightmap!" << endl;
+      gzerr << m_plugin_name << ": onModifyTerrainMsg() could not acquire heightmap." << endl;
       return;
     }
 
@@ -63,41 +70,6 @@ private:
       terrain->updateDerivedData(false, Ogre::Terrain::DERIVED_DATA_NORMALS | Ogre::Terrain::DERIVED_DATA_LIGHTMAP);
 
       m_differential_pub.publish(diff_msg);
-
-      // Override custom material normal map labeled "ow_dynamic_terrain_normal_map"
-      // with Ogre::Terrain's internal normal map.
-      // Ogre::Terrain makes a copy of a user-specified normal map and the copy
-      // is the one that is updated by Terrain::updateDerivedData(), not the
-      // original normal map specified in a custom material file.
-      // Note: If you are not using a custom material this code will have no
-      // effect, but Ogre should be using the correct normal map and a generated
-      // material and shader in that case.
-      // Caveat: The normal map copy is always resized to the terrain's DEM
-      // resolution, so using a larger texture for more detail is not possible
-      // with this plugin. It might be possible to update the original normal map
-      // by computing all necessary heights at the original resolution and then
-      // computing and uploading normals instead of relying on Ogre to do it. 
-      ResourceManager::ResourceMapIterator res_it = MaterialManager::getSingleton().getResourceIterator();
-      while (res_it.hasMoreElements())
-      {
-        ResourcePtr resource = res_it.getNext();
-        MaterialPtr material = resource.staticCast<Material>();
-        Material::TechniqueIterator tech_it = material->getTechniqueIterator();
-        while (tech_it.hasMoreElements())
-        {
-          Technique* technique = tech_it.getNext();
-          Technique::PassIterator pass_it = technique->getPassIterator();
-          while (pass_it.hasMoreElements())
-          {
-            Pass* pass = pass_it.getNext();
-            TextureUnitState* tus = pass->getTextureUnitState("ow_dynamic_terrain_normal_map");
-            if (tus != 0)
-            {
-              tus->setTexture(terrain->getTerrainNormalMap());
-            }
-          }
-        }
-      }
     }
   }
 
@@ -115,6 +87,52 @@ private:
   {
     onModifyTerrainMsg(msg, TerrainModifier::modifyPatch);
   }
+
+  void applyNormalMap()
+  {
+    auto heightmap = getHeightmap(get_scene());
+    if (heightmap == nullptr)
+    {
+      gzerr << m_plugin_name << ": applyNormalMap() could not acquire heightmap." << endl;
+      return;
+    }
+    auto terrain = heightmap->OgreTerrain()->getTerrain(0, 0);
+
+    // Override custom material normal map labeled "ow_dynamic_terrain_normal_map"
+    // with Ogre::Terrain's internal normal map.
+    // Ogre::Terrain generates an internal normal map and it is updated by
+    // Terrain::updateDerivedData(). But if you use a custom material script
+    // with a custom normal map, the internal normal map will never be seen.
+    // Note: If you are not using a custom material this code will have no
+    // effect, but Ogre should be using the correct normal map and a generated
+    // material and shader in that case.
+    // Caveat: The internal normal map is always sized to the terrain's DEM
+    // resolution, so using a larger texture for more detail is not possible
+    // with this plugin.
+    ResourceManager::ResourceMapIterator res_it = MaterialManager::getSingleton().getResourceIterator();
+    while (res_it.hasMoreElements())
+    {
+      ResourcePtr resource = res_it.getNext();
+      MaterialPtr material = resource.staticCast<Material>();
+      Material::TechniqueIterator tech_it = material->getTechniqueIterator();
+      while (tech_it.hasMoreElements())
+      {
+        Technique* technique = tech_it.getNext();
+        Technique::PassIterator pass_it = technique->getPassIterator();
+        while (pass_it.hasMoreElements())
+        {
+          Pass* pass = pass_it.getNext();
+          TextureUnitState* tus = pass->getTextureUnitState("ow_dynamic_terrain_normal_map");
+          if (tus != 0)
+          {
+            tus->setTexture(terrain->getTerrainNormalMap());
+          }
+        }
+      }
+    }
+  }
+
+  gazebo::event::ConnectionPtr m_on_prerender_connection;
 };
 
 GZ_REGISTER_VISUAL_PLUGIN(DynamicTerrainVisual)
