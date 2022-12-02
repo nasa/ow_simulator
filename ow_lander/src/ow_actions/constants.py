7cc1d3a3ba0c544dd774230efa6057e3c4fcb04c
Thomas Stucky <thomas.stucky@nasa.gov>
2022-12-01 18:06:42 -0700

    Removed shebangs because modules do not need them

--- ow_lander/src/ow_actions/constants.py
+++ ow_lander/src/ow_actions/constants.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # The Notices and Disclaimers for Ocean Worlds Autonomy Testbed for Exploration
 # Research and Simulation can be found in README.md in the root directory of
 # this repository.
