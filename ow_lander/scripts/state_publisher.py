70c92b0c4dd9245fb2f876b41e92243d2e4abe00
Terry Welsh <terence.m.welsh@nasa.gov>
2022-12-20 11:48:01 -0800

    Publish end effector position relative to the lander as /arm_pose

--- ow_lander/scripts/state_publisher.py
+++ ow_lander/scripts/state_publisher.py
@@ -5,8 +5,10 @@
 # this repository.
 
 import rospy
+import tf2_ros
 from sensor_msgs.msg import JointState
-from owl_msgs.msg import ArmJointAccelerations, ArmJointPositions, ArmJointTorques, ArmJointVelocities, PanTiltPosition
+from owl_msgs.msg import ArmJointAccelerations, ArmJointPositions, ArmJointTorques, \
+                         ArmJointVelocities, ArmPose, PanTiltPosition
 
 class StatePublisher(object):
 
@@ -16,9 +18,11 @@ class StatePublisher(object):
     self._joint_tor = ArmJointTorques()
     self._joint_vel = ArmJointVelocities()
     self._pan_tilt = PanTiltPosition()
+    self._arm_pose = ArmPose()
 
     self._subscriber = rospy.Subscriber(
             '/joint_states', JointState, self._handle_joint_states)
+
     self._arm_joint_accelerations_pub = rospy.Publisher(
             '/arm_joint_accelerations', ArmJointAccelerations, queue_size=10)
     self._arm_joint_positions_pub = rospy.Publisher(
@@ -29,6 +33,13 @@ class StatePublisher(object):
             '/arm_joint_velocities', ArmJointVelocities, queue_size=10)
     self._pan_tilt_pub = rospy.Publisher(
             '/pan_tilt_position', PanTiltPosition, queue_size=10)
+
+    self._tf_buffer = tf2_ros.Buffer()
+    self._tf_listener = tf2_ros.TransformListener(self._tf_buffer)
+
+    self._arm_pose_pub = rospy.Publisher(
+            '/arm_pose', ArmPose, queue_size=10)
+
 
   def _handle_joint_states(self, data):
     """
@@ -66,6 +77,24 @@ class StatePublisher(object):
     self._pan_tilt.header = data.header
     self._pan_tilt.value = data.position[0 : OFFSET]
     self._pan_tilt_pub.publish(self._pan_tilt)
+
+    # Use /joint_states as the clock for publishing /arm_pose. Alternatively,
+    # this could be refactored put publish /arm_pose at a different rate.
+    self._publish_arm_pose()
+
+
+  def _publish_arm_pose(self):
+    try:
+      trans = self._tf_buffer.lookup_transform('base_link', 'l_scoop',  rospy.Time(0))
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+      rospy.logwarn("StatePublisher: tf2 raised an exception: %s" % e)
+      return
+
+    self._arm_pose.header = trans.header
+    self._arm_pose.value.position = trans.transform.translation
+    self._arm_pose.value.orientation = trans.transform.rotation
+    self._arm_pose_pub.publish(self._arm_pose)
+
 
 if __name__ == '__main__':
     node_name = 'state_publisher'
