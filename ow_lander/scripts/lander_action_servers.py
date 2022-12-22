812dd1bb4149d323463eccc824d678025d1e3bff
Thomas Stucky <thomas.stucky@nasa.gov>
2022-12-22 16:42:39 -0700

    All lander actions are now served by a single node  - Removed spin_action_server since it has no use now

--- ow_lander/scripts/lander_action_servers.py
+++ ow_lander/scripts/lander_action_servers.py
@@ -10,16 +10,23 @@ from ow_lander import actions
 
 rospy.init_node('arm_action_servers')
 
+# arm actions
 server_stop         = actions.StopServer()
+server_guarded_move = actions.GuardedMoveServer()
 server_unstow       = actions.UnstowServer()
 server_stow         = actions.StowServer()
 server_grind        = actions.GrindServer()
-server_guarded_move = actions.GuardedMoveServer()
 server_dig_circular = actions.DigCircularServer()
 server_dig_linear   = actions.DigLinearServer()
 server_discard      = actions.DiscardServer()
 server_deliver      = actions.DeliverServer()
 server_move_joint   = actions.ArmMoveJointServer()
 server_move_joints  = actions.ArmMoveJointsServer()
+
+# other non-arm lander actions
+server_light_set_intensity = actions.LightSetIntensityServer()
+server_camera_capture      = actions.CameraCaptureServer()
+server_dock_ingest_sample  = actions.DockIngestSampleServer()
+server_antenna_pan_tilt    = actions.AntennaPanTiltServer()
 
 rospy.spin()
