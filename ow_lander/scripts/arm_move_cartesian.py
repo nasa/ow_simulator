47cb6671067afb0c8154646b5fc3ccf12409690f
Thomas Stucky <thomas.stucky@nasa.gov>
2023-01-04 13:39:29 -0700

    Improved action client arguments

--- ow_lander/scripts/arm_move_cartesian.py
+++ ow_lander/scripts/arm_move_cartesian.py
@@ -16,30 +16,27 @@ from geometry_msgs.msg import Pose, Point, Quaternion
 parser = argparse.ArgumentParser(
   formatter_class=argparse.ArgumentDefaultsHelpFormatter,
   description="Move end-effector by Cartesian coordinates/translation.")
-parser.add_argument('frame', type=int, nargs='?', default=0,
+parser.add_argument('--frame', '-f', type=int, default=0,
   choices=constants.FRAME_ID_MAP.keys(),
-  help="The frame in which coordinates/translation will be interpreted")
-parser.add_argument('relative', type=strtobool, nargs='?', default=False,
+  help="The frame index corresponding to the frame that orientation and "\
+       " position relative to.")
+parser.add_argument('--relative', '-r', action='store_true', default=False,
   help="If True, pose will be interpreted as relative to the current pose.")
-parser.add_argument('x', type=float, nargs='?', default=0,
+parser.add_argument('-x', type=float, default=0,
   help="X-coordinate or the change in the X-coordinate")
-parser.add_argument('y', type=float, nargs='?', default=0,
+parser.add_argument('-y', type=float, default=0,
   help="Y-coordinate or the change in the Y-coordinate")
-parser.add_argument('z', type=float, nargs='?', default=0,
+parser.add_argument('-z', type=float, default=0,
   help="Z-coordinate or the change in the Z-coordinate")
-parser.add_argument('qx', type=float, nargs='?', default=0,
-  help="First scalar of the quaternion")
-parser.add_argument('qy', type=float, nargs='?', default=0,
-  help="Second scalar of the quaternion")
-parser.add_argument('qz', type=float, nargs='?', default=0,
-  help="Third scalar of the quaternion")
-parser.add_argument('qw', type=float, nargs='?', default=1,
-  help="Fourth scalar of the quaternion")
+parser.add_argument('--orientation', '-o', type=float, nargs=4,
+  default=[0, 0, 0, 1], metavar=('X', 'Y', 'Z', 'W'),
+  help="Orientation in quaternion form. Takes 4 scalars, the first 3 are the" \
+       " imaginary parts, and the 4th is the real part.")
 args = parser.parse_args()
 
 pose_arg = Pose(
   position=Point(args.x, args.y, args.z),
-  orientation=Quaternion(args.qx, args.qy, args.qz, args.qw)
+  orientation=Quaternion(*args.orientation)
 )
 
 node_helper.call_single_use_action_client(actions.ArmMoveCartesianServer,
