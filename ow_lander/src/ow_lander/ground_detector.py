7cae50d8b1ecbd486a0487d1a4a23e5fea5994fd
Anthony Koutroulis <anthony.koutroulis@nasa.gov>
2023-01-19 08:08:42 -0800

    update references and topic redirects to unified standard

--- ow_lander/src/ow_lander/ground_detector.py
+++ ow_lander/src/ow_lander/ground_detector.py
@@ -4,7 +4,8 @@
 
 import rospy
 import tf2_ros
-from geometry_msgs.msg import Point, WrenchStamped
+from geometry_msgs.msg import Point
+from owl_msgs.msg import ArmEndEffectorForceTorque
 
 class GroundDetector:
   """GroundDetector uses readings of the force torque sensor to detect when the
@@ -12,8 +13,8 @@ class GroundDetector:
   """
   def __init__(self, reference_frame, poker_link):
     self._detected = False
-    self._ft_sensor_sub = rospy.Subscriber('/ft_sensor_dist_pitch',
-                                           WrenchStamped,
+    self._ft_sensor_sub = rospy.Subscriber('/arm_end_effector_force_torque',
+                                           ArmEndEffectorForceTorque,
                                            self._ft_sensor_cb)
     self._buffer = tf2_ros.Buffer()
     self._listener = tf2_ros.TransformListener(self._buffer)
@@ -22,12 +23,13 @@ class GroundDetector:
 
   def _ft_sensor_cb(self, msg):
     """Checks if force threshold has been reached
-    msg -- Instance of geometry_msgs.msg.WrenchStamped
+    msg -- Instance of ArmEndEffectorForceTorque.msg.value, a float64[6]
+             where indices [0-2] are the force vector
     """
     # TODO: could this be made more robust by looking at force magnitude instead
     #       of just the x-component?
     FORCE_X_THRESHOLD = -100.0
-    if msg.wrench.force.x < FORCE_X_THRESHOLD:
+    if msg.value[0] < FORCE_X_THRESHOLD:
       self._detected = True
 
   def was_ground_detected(self):
