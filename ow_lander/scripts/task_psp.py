69698bef2b243aacc2a330563ebd2c72ae0fdcd5
Michael Dalal <michael.dalal@nasa.gov>
2023-01-20 10:16:39 -0800

    started TaskPSP

---
+++ ow_lander/scripts/task_psp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# The Notices and Disclaimers for Ocean Worlds Autonomy Testbed for Exploration
+# Research and Simulation can be found in README.md in the root directory of
+# this repository.
+
+from ow_lander import actions
+from ow_lander import node_helper
+
+import argparse
+
+parser = argparse.ArgumentParser(
+  formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+  description="Use the Pressure Sinkage Plate (UNSUPPORTED)."
+)
+parser.add_argument(
+  'tool', type=int, nargs='?', default=0,
+  help="Tool choice (0 = Scoop, 1 = Drill)"
+)
+args = parser.parse_args()
+
+node_helper.call_single_use_action_client(
+  actions.ArmSetToolServer, **vars(args)
+)
